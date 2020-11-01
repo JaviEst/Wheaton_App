6c9cd6a3bf80801b54e9c1290218db30b881f447
itracy99 <61026317+itracy99@users.noreply.github.com>
2020-11-01 16:25:21 -0500

    Update map_fragment.java

--- app/src/main/java/com/example/wheaton/map_fragment.java
+++ app/src/main/java/com/example/wheaton/map_fragment.java
@@ -1,4 +1,29 @@
 package com.example.wheaton;
 
+<<<<<<< HEAD
 public class map_fragment {
+=======
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class map_fragment extends Fragment {
+
+    // Declare view
+    View root_view;
+
+    public map_fragment(){}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        root_view = inflater.inflate(R.layout.map, container, false);
+        return root_view;
+    }
+>>>>>>> First_Year
 }
