5aa8a045dd21a9ae296bd05cb79f4dcf9ede73cb
Alexis <aromer4444@gmail.com>
2020-11-03 16:47:18 -0500

    putting layout into local athletics

--- app/src/main/java/com/example/wheaton/MainActivity.java
+++ app/src/main/java/com/example/wheaton/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.wheaton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void directoryButton(View view) {
+        Intent urlIntent = new Intent(Intent.ACTION_VIEW);
+        urlIntent.setData(Uri.parse("https://wheatoncollegelyons.com/information/directory/index"));
+        if (urlIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(urlIntent);
+        }
+        startActivity(urlIntent);
+
+    }
+
+
+
+
 }
