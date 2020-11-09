1e22329ef21b102f23de0b7ae96810a6bcc652fe
Albert Campos <albertapple99@gmail.com>
2020-11-09 14:12:46 +0100

    Finally sorted my problems with changing activities
    Balfour activity set up and running

--- app/src/main/java/com/example/wheaton/Dining/BalfourActivity.java
+++ app/src/main/java/com/example/wheaton/Dining/BalfourActivity.java
@@ -4,16 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+
 import com.example.wheaton.R;
 
-
 public class BalfourActivity extends AppCompatActivity {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_balfour);
 
         Intent call_intent = getIntent();
     }
-
 }
