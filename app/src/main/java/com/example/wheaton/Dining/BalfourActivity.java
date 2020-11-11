b3391e178d39e432a03b78fddd61382eaa39881a
Albert Campos <albertapple99@gmail.com>
2020-11-11 15:57:59 +0100

    Setting up Dining Facilities Activities
    Finished setting up the webviews for the dining facilities activities

--- app/src/main/java/com/example/wheaton/Dining/BalfourActivity.java
+++ app/src/main/java/com/example/wheaton/Dining/BalfourActivity.java
@@ -4,15 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+
 
 import com.example.wheaton.R;
 
 public class BalfourActivity extends AppCompatActivity {
 
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_balfour);
+
+        webView = (WebView) findViewById(R.id.BalfourWebView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://wheatoncollege.campusdish.com/LocationsAndMenus/HoodEcoCafe");
+
+        WebSettings webSettings = webView.getSettings();
 
         Intent call_intent = getIntent();
     }
--- app/src/main/java/com/example/wheaton/Dining/ChaseActivity.java
+++ app/src/main/java/com/example/wheaton/Dining/ChaseActivity.java
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.wheaton.R;
 
 public class ChaseActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chase);
+
+        webView = (WebView) findViewById(R.id.ChaseWebView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://wheatoncollege.campusdish.com/LocationsAndMenus/ChaseDiningHall");
+
+        WebSettings webSettings = webView.getSettings();
 
         Intent call_intent = getIntent();
     }
